765dae5e8d05c89340661a1410ac3d48e1fc2821
DavidParkerDr <david@goparker.com>
2022-06-21 14:45:12 +0100

    serveClient: false

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path          = require('path');
 const http          = require('http');
 const express       = require('express');
-const socketIO      = require('socket.io');
+const socketIO      = require('socket.io')(server, {serveClient: false});
 
 const publicPath    = path.join(__dirname, '/../public');
 
